b31827fd10baf6d71a351dc04caa777aee302ea5
Adam Ever-Hadani <adamhadani@adamhadani-laptop.(none)>
2010-07-09 17:53:26 -0700

    add --ignore flag to logparse

--- logtools/_parse.py
+++ logtools/_parse.py
@@ -22,6 +22,7 @@ import sys
 import logging
 from itertools import imap
 from functools import partial
+from operator import and_
 from optparse import OptionParser
 
 import logtools.parsers
@@ -30,17 +31,28 @@ from _config import logtools_config, interpolate_config, AttrDict
 __all__ = ['logparse_parse_args', 'logparse', 'logparse_main']
 
 
-def multikey_getter_gen(parser, keys, delimiter="\t"):
+def multikey_getter_gen(parser, keys, is_indices=False, delimiter="\t"):
     """Generator meta-function to return a function
     parsing a logline and returning multiple keys (tab-delimited)"""
-    if not hasattr(keys, 'intersection'):
+    if is_indices:
+        keys = map(int, keys)
+    elif not hasattr(keys, 'intersection'):
         keys = set(keys)
         
     def multikey_getter(line, parser, keyset):
         data = parser(line.strip())
-        return delimiter.join((data[k] for k in keyset.intersection(data)))
+        return delimiter.join((data[k] for k in keyset))
+    
+    def multiindex_getter(line, parser, keyset):
+        data = parser(line.strip())
+        return delimiter.join((data.by_index(idx, raw=True) for idx in keys))
 
-    return partial(multikey_getter, parser=parser, keyset=keys)
+    if is_indices is True:
+        # Field indices
+        return partial(multiindex_getter, parser=parser, keyset=keys)
+    else:
+        # Field names
+        return partial(multikey_getter, parser=parser, keyset=keys)
 
 
 def logparse_parse_args():
@@ -51,6 +63,8 @@ def logparse_parse_args():
                     help="Format string. Used by the parser (e.g AccessLog format specifier)")    
     parser.add_option("-f", "--field", dest="field", default=None,
                     help="Parsed Field index to output")    
+    parser.add_option("-i", "--ignore", dest="ignore", default=None, action="store_true",
+                    help="Ignore missing fields errors (skip lines with missing fields)")    
     
     parser.add_option("-P", "--profile", dest="profile", default='logparse',
                       help="Configuration profile (section in configuration file)")
@@ -59,8 +73,11 @@ def logparse_parse_args():
     
     # Interpolate from configuration
     options.parser = interpolate_config(options.parser, options.profile, 'parser') 
-    options.format = interpolate_config(options.format, options.profile, 'format', default=False) 
+    options.format = interpolate_config(options.format, options.profile, 'format', 
+                                        default=False) 
     options.field = interpolate_config(options.field, options.profile, 'field')
+    options.ignore = interpolate_config(options.ignore, options.profile, 'ignore', 
+                                        default=False, type=bool)
     
 
     return AttrDict(options.__dict__), args
@@ -91,10 +108,18 @@ def logparse(options, args, fh):
             key_func = lambda x: parser(x.strip())[field]
         else:
             # Multiple fields requested
-            key_func = multikey_getter_gen(parser, keys)
+            is_indices = reduce(and_, (k.isdigit() for k in keys), True)
+            key_func = multikey_getter_gen(parser, keys, is_indices=is_indices)
     
     for line in fh:
-        yield key_func(line)
+        try:
+            yield key_func(line)
+        except KeyError:
+            if options.ignore:
+                logging.debug("Could not match fields for parsed line: %s", line)
+                continue
+            else:
+                raise
     
 def logparse_main():
     """Console entry-point"""

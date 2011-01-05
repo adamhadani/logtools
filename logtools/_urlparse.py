1db5130972713e676ac5c00e0f27cb8fa31bbff9
Adam Ever-Hadani <adamhadani@gmail.com>
2011-01-05 10:46:57 -0800

    urlparse can now take multiple query params to print. upaded unit-tests as well. Enabled doctest in setup.cfg

--- logtools/_urlparse.py
+++ logtools/_urlparse.py
@@ -92,9 +92,12 @@ def urlparse_main():
     options, args = urlparse_parse_args()
     for parsed_url in urlparse(fh=sys.stdin, *args, **options):
         if parsed_url:
-            print >> sys.stdout, parsed_url
+            if isinstance(parsed_url, list):
+                # Format as tab-delimited for output
+                parsed_url = "\t".join(parsed_url)
+            print parsed_url
         else:
             # Lines where we couldnt get any match (e.g when using -q)
-            print >> sys.stdout, ''
+            print ''
 
     return 0

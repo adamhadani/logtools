4ee82a1f2bd1a79c9e2aa1432a926fbf72567120
Adam Ever-Hadani <adamhadani@gmail.com>
2011-07-28 14:30:16 -0700

    changelog/version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except ImportError:
 
 setup(  
     name         = 'logtools',
-    version      = '0.7',
+    version      = '0.7.1',
     description  = 'Log analysis and filtering tools',
     author       = 'Adam Ever-Hadani',
     author_email = 'adamhadani@gmail.com',

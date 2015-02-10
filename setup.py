7093425d6a572fd8862f55a0f930f7747efa2792
Adam Ever-Hadani <adamhadani@gmail.com>
2015-02-09 21:26:59 -0800

    0.8.7 version tag in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except ImportError:
 
 setup(
     name         = 'logtools',
-    version      = '0.8.6',
+    version      = '0.8.7',
     description  = 'Log analysis and filtering tools',
     author       = 'Adam Ever-Hadani',
     author_email = 'adamhadani@gmail.com',
